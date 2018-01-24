42455c8f65343a34e1de1455902c54dbb84ac4b2
depressiveman <31856062+depressiveman@users.noreply.github.com>
2018-01-24 07:52:35 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,27 @@
+namespace robloxplayer239
+{
+	int rand() {
+		return 5;
+	}
+}
+
+#include <iostream>
+#include<time.h>
+
+int main() {
+	srand(time(NULL));
+	int randNum;
+	int fakeRandNum;
+
+	randNum = rand();
+	fakeRandNum = robloxplayer239::rand();
+
+	std::cout << randNum << std::endl;
+	std::cout << fakeRandNum << std::endl;
+	system("pause");
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+}
